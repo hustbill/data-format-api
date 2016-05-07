60a1a514e927d190e2c88275a9f2271af334b1f4
Hua Zhang <hua.zhang@wsu.edu>
2016-05-06 21:15:38 -0700

    Read Json data from String

--- jsonParser/src/main/java/jsonParser/JsonExample.java
+++ jsonParser/src/main/java/jsonParser/JsonExample.java
@@ -119,53 +119,7 @@ public class JsonExample {
 
 	}
 
-	public static void readJson(String[] args) throws Exception {
-				String personJSONData = "  {" + "       \"name\": \"John\", " + "       \"age\" : 35, "
-				+ "       \"isMarried\" : true, " + "       \"email\": null, " + "       \"address\": { "
-				+ "           \"street\": \"#234, Pembroke Road\", " + "           \"city\": \"Dublin\", "
-				+ "           \"zipCode\": \"D4\" " + "       }, "
-				+ "       \"phoneNumbers\": [\"89-923-2342\", \"89-213-2364\"] " + "   }";
-		JsonParser parser = Json.createParser(new StringReader(personJSONData));
-		
-		while (parser.hasNext()) {
-			Event event = parser.next();
-			switch (event) {
-			case START_OBJECT:
-				System.out.print("{/*" + event + "*/ ");
-				break;
-			case END_OBJECT:
-				System.out.print("}/*" + event + "*/ ");
-				break;
-			case START_ARRAY:
-				System.out.print("[/*" + event + "*/ ");
-				break;
-			case END_ARRAY:
-				System.out.print("]/*" + event + "*/ ");
-				break;
-			case VALUE_NUMBER:
-				System.out.print(parser.getInt() + "/*" + event + "*/, ");
-				break;
-			case VALUE_STRING:
-				System.out.print(parser.getString() + "/*" + event + "*/, ");
-				break;
-			case VALUE_TRUE:
-				System.out.print("true/*" + event + "*/, ");
-				break;
-			case VALUE_FALSE:
-				System.out.print("false/*" + event + "*/, ");
-				break;
-			case VALUE_NULL:
-				System.out.print("null/*" + event + "*/, ");
-				break;
-			case KEY_NAME:
-				System.out.print(parser.getString() + "/*" + event + "*/: ");
-				break;
-			default:
-				System.out.print("Unrecognized Event : ");
-			}
-		}
-	}
-
+	
 }
 
 /*
