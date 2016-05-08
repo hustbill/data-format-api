b45bf83570d9e16d63f7848babcf72d88f7759bb
Hua Zhang <hua.zhang@wsu.edu>
2016-05-07 23:39:35 -0700

    Add Date Format package

---
+++ dataFormatter/src/main/java/date/Test.java
@@ -0,0 +1,57 @@
+package date;
+
+import java.util.*;
+import java.util.regex.*;
+
+class ReformatDate {
+
+	public static void main(String[] argh) {
+		Parser X = new Parser();
+		Scanner in = new Scanner(System.in);
+
+		while (in.hasNext()) {
+			System.out.println(X.checkParenthesisBalance(in.next()));
+		}
+
+	}
+
+	public static boolean checkParenthesisBalance(String input) {
+		HashMap<Character, Character> map = new HashMap<Character, Character>() {
+			{
+				put('(', ')');
+				put('{', '}');
+			}
+		};
+
+		Stack<Character> st = new Stack<Character>();
+		for (char c : input.toCharArray()) {
+			if (map.containsKey(c)) {
+				st.push(c);
+			} else if (st.isEmpty() || map.get(st.pop()) != c) {
+				return false;
+			}
+		}
+		return st.isEmpty();
+	}
+}
+
+class Parser {
+	public boolean checkParenthesisBalance(String input) {
+		HashMap<Character, Character> map = new HashMap<Character, Character>() {
+			{
+				put('(', ')');
+				put('{', '}');
+			}
+		};
+
+		Stack<Character> st = new Stack<Character>();
+		for (char c : input.toCharArray()) {
+			if (map.containsKey(c)) {
+				st.push(c);
+			} else if (st.isEmpty() || map.get(st.pop()) != c) {
+				return false;
+			}
+		}
+		return st.isEmpty();
+	}
+}
